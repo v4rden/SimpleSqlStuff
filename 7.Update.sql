203691bef4253c016c8500177b15f2b5b88d08bf
v4rden <denys.variah@gmail.com>
2019-08-09 18:36:42 +0300

    Comments

--- 7.Update.sql
+++ 7.Update.sql
@@ -1,8 +1,10 @@
+--It is supposed you run InsertInto.sql
 USE AdventureWorksLT2017
 UPDATE SalesLT.Address
 SET City = 'UpdatedCity'
 WHERE City = 'TestCity';
 
+--Check
 SELECT *
 from SalesLT.Address
 WHERE City = 'UpdatedCity'
