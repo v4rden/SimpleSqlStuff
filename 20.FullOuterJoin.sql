dc216678a50ed5792000e914a4b3da00f9684cce
v4rden <denys.variah@gmail.com>
2019-08-10 14:59:24 +0300

    Full outer join

---
+++ 20.FullOuterJoin.sql
@@ -0,0 +1,11 @@
+USE AdventureWorksLT2017
+
+SELECT p.Name,
+       p.StandardCost,
+       p.ListPrice,
+       sod.UnitPrice,
+       sod.UnitPriceDiscount
+FROM SalesLT.SalesOrderDetail sod
+         FULL OUTER JOIN SalesLT.Product p
+                         ON sod.ProductID = p.ProductID
+WHERE sod.UnitPrice > 1000;
